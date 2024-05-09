1059133257eb831a3178ae424c1df07572e03f91
sangy <apple2951@gmail.com>
2024-05-09 21:45:32 +0800

    feaure:New post feature

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -6,6 +7,7 @@ class Post(models.Model):
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
     # blank=True means not required
     banner = models.ImageField(default="fallback.png", blank=True)
